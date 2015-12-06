516992d47c513a05a19124b4bdb28ce2f9ef6d38
apatrone <agnespatrone@hotmail.com>
2015-12-06 18:57:24 -0300

    works but precision error
    maybe because of difference in rounding on gpu vs cpu

--- Practica/entrega1_2_lasttry.cu
+++ Practica/entrega1_2_lasttry.cu
@@ -11,14 +11,14 @@ b.Tanto C como N deben estar almacenados en la memoria de constantes de la GPU*/
 #include <math.h>
 
 
-float secuential(const float a[] , int dim,bool verbose){
-	float mean=0;
+double secuential(const double a[] , int dim,bool verbose){
+	double mean=0;
 	for(int i=0; i<dim;i++){
 		mean+=a[i];
 	}
 	mean=mean/dim;
 	if(verbose)printf("cpu mean %f\n", mean);
-	float sum=0;
+	double sum=0;
 	for(int i=0; i<dim;i++){
 		sum+=(a[i]-mean)*(a[i]-mean);
 	}
@@ -28,7 +28,7 @@ float secuential(const float a[] , int dim,bool verbose){
 }
 
      
-__global__ void reduccion( float a[], const int dim)
+__global__ void reduccion( double a[], const int dim)
 {
 	unsigned long int i;
 	unsigned long int global_id = blockIdx.x * blockDim.x + threadIdx.x;	
@@ -40,7 +40,7 @@ __global__ void reduccion( float a[], const int dim)
 	}
 } 
 
-__global__ void pre_sigma( float a[], const int dim, const float mean)
+__global__ void pre_sigma( double a[], const int dim, const double mean)
 {
 	unsigned long int global_id = blockIdx.x * blockDim.x + threadIdx.x;	
 
@@ -49,7 +49,6 @@ __global__ void pre_sigma( float a[], const int dim, const float mean)
 		a[global_id]*= a[global_id];
 	}
 
-	
 } 
 
     
@@ -58,33 +57,31 @@ int main(int argc, char *argv[]){
 	clock_t time_begin;
 	
     // pointers to host & device arrays
-     float *device_array = 0;
-     float *host_array = 0;
+     double *device_array = 0;
+     double *host_array = 0;
 
 	unsigned long int size_array  = (argc > 1)? atoi (argv[1]): 1024;
 	unsigned int block_size = (argc > 2)? atoi (argv[2]): 16;	
 	bool verbose= (argc>3)? (argv[3][0]=='v'): false;
 
       // malloc a host array
-     host_array = (float*)malloc( size_array * sizeof(float));
-	 float *copy_host_array=(float*)malloc( size_array * sizeof(float));
+     host_array = (double*)malloc( size_array * sizeof(double));
+	 double *copy_host_array=(double*)malloc( size_array * sizeof(double));
 	
-    for(int i=0; i<size_array; i++){
+    for(unsigned int i=0; i<size_array; i++){
 		host_array[i]=rand()%10;
 		copy_host_array[i]=host_array[i];
-		if(argc==4 && verbose){
+		if(verbose)
 			printf("%f\t", host_array[i]);
-		}
-		else if(argc==4)
-			host_array[i]=atoi(argv[3]);
+
     }
     printf("\n");	
 	
      // cudaMalloc a device array
-     cudaMalloc(&device_array,size_array * sizeof(int));    
+     cudaMalloc(&device_array,size_array * sizeof(double));    
 
     // download and inspect the result on the host:
-    cudaError_t e=cudaMemcpy(device_array, host_array, sizeof(int)*size_array, cudaMemcpyHostToDevice); 
+    cudaError_t e=cudaMemcpy(device_array, host_array, sizeof(double)*size_array, cudaMemcpyHostToDevice); 
 
 	//cudaerrorinvalidvalue(11)
 
@@ -100,11 +97,11 @@ int main(int argc, char *argv[]){
 	}
 
     // download and inspect the result on the host:
-	cudaMemcpy(copy_host_array, device_array, sizeof(int)*size_array, cudaMemcpyDeviceToHost); 
+	cudaMemcpy(copy_host_array, device_array, sizeof(double)*size_array, cudaMemcpyDeviceToHost); 
 	
-	float mean_gpu= copy_host_array[0] / size_array;
+	double mean_gpu= copy_host_array[0] / size_array;
 	//copy again the original array to the device array which had been modified
-	e=cudaMemcpy(device_array, host_array, sizeof(int)*size_array, cudaMemcpyHostToDevice); 
+	e=cudaMemcpy(device_array, host_array, sizeof(double)*size_array, cudaMemcpyHostToDevice); 
 
 	dim3 bloque2(block_size);	
 	dim3 grid2((size_array + bloque2.x - 1) / bloque2.x);			
@@ -112,11 +109,11 @@ int main(int argc, char *argv[]){
 	pre_sigma<<<bloque2, grid2>>>(device_array, size_array, mean_gpu);
 
 	cudaThreadSynchronize();
-	cudaMemcpy(copy_host_array, device_array, sizeof(int)*size_array, cudaMemcpyDeviceToHost); 
+	cudaMemcpy(copy_host_array, device_array, sizeof(double)*size_array, cudaMemcpyDeviceToHost); 
 
 	if(verbose){
 		printf("mean gpu: %f\n", mean_gpu);
-		for(int j=0; j<size_array; j++)
+		for(unsigned int j=0; j<size_array; j++)
 			printf("%f\t", copy_host_array[j]);
 		printf("\n");
 	}
@@ -130,18 +127,18 @@ int main(int argc, char *argv[]){
 		i = i/2;
 	}
 	
-	cudaMemcpy(copy_host_array, device_array, sizeof(int)*size_array, cudaMemcpyDeviceToHost); 
+	cudaMemcpy(copy_host_array, device_array, sizeof(double)*size_array, cudaMemcpyDeviceToHost); 
 
 	if(verbose) printf("gpu sigma %f \n", copy_host_array[0]);
-	float final_res= sqrt(copy_host_array[0]/(size_array-1));
+	double final_res= sqrt(copy_host_array[0]/(size_array-1));
 
-	printf("GPU time: %f seconds\t", (((float)clock() - (float)time_begin) / 1000000.0F ) * 1000  ); 
+	printf("GPU time: %f seconds\t", (((double)clock() - (double)time_begin) / 1000000.0F ) * 1000  ); 
 	printf("GPU result: %f\n", final_res);
 
 	//--------------cpu computations-----------------------//
 	time_begin=clock();
-	float cpu_res=secuential(host_array, size_array, verbose);
-	printf("CPU time: %f seconds\t", (((float)clock() - (float)time_begin) / 1000000.0F ) * 1000  );
+	double cpu_res=secuential(host_array, size_array, verbose);
+	printf("CPU time: %f seconds\t", (((double)clock() - (double)time_begin) / 1000000.0F ) * 1000  );
 	printf("CPU result: %f\n", cpu_res);
 
     // deallocate memory
